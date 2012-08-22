ffb8dde365878890cc325b21970f513921e290cc
fozziethebeat <fozziethebeat@gmail.com>
2012-08-22 16:41:21 +0900

    Adding tex for a presentation on twitter summarization

---
+++ TwitterEval/src/main/scala/edu/ucla/sspace/EvaluateSegmentsAgainstEvents.scala
@@ -0,0 +1,25 @@
+package edu.ucla.sspace
+
+import scala.io.Source
+import scala.math.abs
+
+
+object EvaluateSegmentsAgainstEvents {
+    def main(args: Array[String]) {
+        val segmentIter = Source.fromFile(args(0)).getLines
+        segmentIter.next
+        val segmentList = segmentIter.map(_.split(",", 4)(0).toLong).toSet.toList.sorted
+        val eventList = Source.fromFile(args(1)).getLines.map(_.toLong).toSet.toList.sorted
+
+        val segmentError = eventList.map(event => abs(event - findBestSegment(event, segmentList.head, segmentList.tail))).sum
+        println(segmentError)
+    }
+
+    def findBestSegment(eventTime: Long, bestTime: Long, segmentList: List[Long]) : Long =
+        segmentList match {
+            case head::tail => if (head > eventTime) if ( abs(bestTime - eventTime) < abs(head - eventTime) ) bestTime 
+                                                     else eventTime
+                               else findBestSegment(eventTime, head, tail)
+            case emptyList => bestTime
+        }
+}
