e63641e770959f5255d8ccc34ea9067ef44d47fd
fozziethebeat <fozziethebeat@gmail.com>
2012-08-23 13:54:21 +0900

    Changing the minute counter to include counts for non observed minutes, trying to debug and fix the bayesian particle filter, Adding a script to munge bcp output into the needed csv format

--- TwitterEval/src/main/scala/edu/ucla/sspace/MinuteCounter.scala
+++ TwitterEval/src/main/scala/edu/ucla/sspace/MinuteCounter.scala
@@ -8,7 +8,7 @@ import java.io.PrintWriter
 object MinuteCounter {
     def main(args:Array[String]) {
         val tweetIter = Source.fromFile(args(0)).getLines
-        tweetIter.next
+//        tweetIter.next
         val tweetTimes = tweetIter.map(_.split("\\s+")(0).toDouble.toInt)
         var lastMinute = 0
         var minuteCount = 0
@@ -17,8 +17,11 @@ object MinuteCounter {
         for ( time <- tweetTimes) {
             val minute = time / 60
             if (minute != lastMinute) {
-                if (lastMinute > 0)
+                if (lastMinute > 0) {
                     writer.println("%d %d".format(lastMinute, minuteCount))
+                    for (m <- lastMinute+1 until minute)
+                        writer.println("%d 0".format(m))
+                }
                 lastMinute = minute
                 minuteCount = 0
             }
