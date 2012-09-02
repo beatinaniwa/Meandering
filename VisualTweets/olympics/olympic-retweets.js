d5e41a72d26b43217feb756aafc1dfe10d333863
fozziethebeat <fozziethebeat@gmail.com>
2012-09-02 13:01:49 +0900

    Adding css and js sub directories to host used files.  Trying to pretty up graph movement by including zoom and panning

--- VisualTweets/olympics/olympic-retweets.js
+++ VisualTweets/olympics/olympic-retweets.js
@@ -13,19 +13,40 @@ var force = d3.layout.force()
     .linkDistance(30)
     .size([w, h - 160]);
 
-var vis;
+var vis, dx = 0, dy = 0, zoomScale = 1.0;
 
 var divisor = 1000 * // Miliseconds per second
               60 * // Seconds per minute
               30 * // Minutes per hour
               1; // Hours we want to divide by
 
+function moveSvg() {
+    dx += d3.event.dx;
+    dy += d3.event.dy;
+    x = dx + (w /2 - w * zoomScale / 2);
+    y = dy + (h /2 - h * zoomScale / 2);
+    vis.attr("transform", "translate(" + [x, y] +") scale(" + zoomScale + ")");
+}
+
+function zoomTranslate(zoom) {
+    zoomScale = zoom;
+    x = dx + (w /2 - w * zoomScale / 2);
+    y = dy + (h /2 - h * zoomScale / 2);
+    vis.attr("transform", "translate(" + [x, y] +") scale(" + zoomScale + ")");
+}
+
 function reload() {
+    dx = 0;
+    dy = 0;
+    zoomScale = 1.0;
+    d3.select("#altZoom").property("value", "20");
+
     var sport = activeLabel("sports");
     var file = "/data/retweet." + sport + ".json";
     d3.json(file, function(json) {
         json.nodes.forEach(function(d, i) {
             d.value = +d.value;
+            d.currCount = 0;
         });
         json.links.forEach(function(d, i) {
             d.value = +d.value;
@@ -43,7 +64,9 @@ function update(userList, linkList) {
     vis = d3.select("#graph")
             .append("svg:svg")
             .attr("width", w)
-            .attr("height", h);
+            .attr("height", h)
+            .call(d3.behavior.drag().on("drag", moveSvg))
+            .append("g");
 
     var nodes = userList, links = linkList;
     // Restart the force layout.
@@ -61,10 +84,18 @@ function update(userList, linkList) {
 
     var charts = [
         barChart().dimension(time)
-                  .group(times)
-                  .x(d3.scale.linear()
-                             .domain([minTime, maxTime])
-                             .rangeRound([0, 40 * 20])),
+                 .group(times)
+                 .x(d3.scale.linear()
+                            .domain([minTime, maxTime])
+                            .rangeRound([0, 500]))
+                 .y(d3.scale.linear().range([30,0])),
+        /*
+        verBarChart().dimension(time)
+                     .group(times)
+                     .x(d3.scale.linear()
+                                .domain([minTime, maxTime])
+                                .rangeRound([0, 600])),
+                                */
     ];
 
     var chart = d3.selectAll(".chart")
@@ -101,12 +132,21 @@ function update(userList, linkList) {
         d3.select("#active").text(formatNumber(all.value()));
     }
 
+    function updateUser(user, userSet) {
+        if (user.index in userSet) {
+            user.currCount += 1;
+        } else {
+            user.currCount = 1;
+            userSet[user.index] = true;
+        }
+    }
+
     function linkGraph(div) {
         var activeLinks = time.top(Infinity);
         var userSet = {};
         activeLinks.forEach(function(d, i) {
-            userSet[d.source.index] = true;
-            userSet[d.target.index] = true;
+            updateUser(d.source, userSet);
+            updateUser(d.target, userSet);
         });
         var activeNodes = [];
         for (activeNodeIndex in userSet)
@@ -133,14 +173,14 @@ function update(userList, linkList) {
                   .style("fill", "#fd8d3c");
 
         node.transition()
-            .attr("r", function(d) { return Math.sqrt(d.value); });
+            .attr("r", function(d) { return Math.log(d.currCount); });
 
         // Enter any new nodes.
         node.enter().append("svg:circle")
             .attr("class", "node")
             .attr("cx", function(d) { return d.x; })
             .attr("cy", function(d) { return d.y; })
-            .attr("r", function(d) { return Math.sqrt(d.value); })
+            .attr("r", function(d) { return Math.log(d.currCount); })
             .style("fill", "#fd8d3c")
             .call(force.drag);
 
@@ -161,6 +201,20 @@ function tick() {
   node.attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; });
 }
+
+$( "#slider-vertical" ).slider({
+    orientation: "vertical",
+    range: "min",
+    min: 0,
+    max: 100,
+    value: 20,
+    slide: function( event, ui ) {
+        var value = ui.value;
+        if (value == 0)
+            value = 1;
+        zoomTranslate(value/20.0);
+    }
+});
 
 setupControl("sports", reload);
 reload();
